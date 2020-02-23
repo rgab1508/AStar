718b5948f6263a4607aa0d25ce86c754658d1cf2
Gabriel <rgabriel1508@gmial.com>
2020-02-24 00:10:51 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import random
 from graphics.graphics import *
 
 ## BRUHH
-
+WIN = None
 WIDTH = 600
 HEIGHT = 600
-ROWS = 50
-COLS = 50
+ROWS = 10
+COLS = 10
 START = Point(0, 0)
-END = Point(40, 30)
+END = Point(1, 1)
 WX = WIDTH/ROWS
 WY = HEIGHT/COLS
 SOLVED = False
+
 
 grid = []
 
@@ -29,7 +30,7 @@ class Node():
         self.end = False
         self.previous = None
         self.neighbours = []
-        self.obstacle = True if random.random() < 0.25 else False
+        self.obstacle = False # True if random.random() < 0.25 else False
         self.path = False
         self.color = self.getColor()
         self.r = Rectangle(Point(self.x, self.y), Point(self.x + WX-1, self.y + WY-1))
@@ -46,18 +47,18 @@ class Node():
         else:
             return "white"
 
-    def show(self, win):
+    def show(self, WIN):
         self.r.setFill(self.getColor())
-        self.r.setOutline(self.getColor())
-        self.r.draw(win)
+        # self.r.setOutline(self.getColor())
+        self.r.draw(WIN)
 
 
     def undraw(self):
         self.r.undraw()
 
-    def redraw(self, win):
+    def redraw(self, WIN):
         self.undraw()
-        self.show(win)
+        self.show(WIN)
 
 
     def setColor(self, color):
@@ -85,6 +86,9 @@ class Node():
     def distFrom(a, b):
         return math.sqrt((b.i - a.i)**2 + (b.j - a.j)**2)
 
+    def contains(self, p):
+        return (((p.x > self.x) and (p.x < self.x + WX -1)) and ((p.y > self.y) and(p.y < self.y + WY -1)))
+
     def get_neighbours(self):
         for i in range(max(0, self.i - 1), min(ROWS, self.i + 2)):
             for j in range(max(0, self.j - 1), min(COLS, self.j + 2)):
@@ -92,12 +96,84 @@ class Node():
                     self.neighbours.append(grid[i][j])
 
 
+def draw_path(path):
+    # print(len(path))
+    for p in path:
+        p.undraw()
+        p.path = True
+        p.redraw(WIN)
+        # print(p.i, p.j)
+        # p.show(WIN)
 
+def construct_path(current):
+    path = []
+    path.append(current)
+    current =  current
+    while current.previous:
+        path.insert(0, current.previous)
+        current = current.previous
+
+    draw_path(path)
+
+def getLowest(openSet):
+    lowest = float("inf")
+    lowestNode = None
+    for i in range(len(openSet)):
+        if openSet[i].getF() < lowest:
+            lowestNode = openSet[i]
+        
+    return lowestNode
+
+def Astar():
+    print("A starring...")
+    openSet = []
+    closedSet = []
+    openSet.append(grid[int(START.x)][int(START.y)])
+    
+    # print(len(openSet))
+    grid[int(START.x)][int(START.y)].setF(grid[int(START.x)][int(START.y)].getG()+grid[int(START.x)][int(START.y)].getH())  
+    while len(openSet) > 0:
+        global SOLVED
+        
+        current = getLowest(openSet)
+        if current.i == max(0, END.x-1) and current.j == max(0, END.y-1):
+            SOLVED = True
+            print("**A starred**")
+            
+            return construct_path(current)
+            
+        
+        openSet.remove(current)
+        closedSet.append(current)
+        for neighbour in current.neighbours:
+            if neighbour in closedSet:
+                continue
+            
+            tempG = current.getG() + 1#Node.distFrom(current, neighbour)
+
+            # if tempG < neighbour.getG():
+            #     neighbour.previous = current
+            #     neighbour.setG(value=tempG)
+            #     neighbour.setF(neighbour.getG() + neighbour.getH())
+
+            #     if neighbour not in openSet and not neighbour.obstacle:
+            #         openSet.append(neighbour)
+            if neighbour in closedSet and tempG >= neighbour.getG():
+                    continue
+            if neighbour not in closedSet or tempG < neighbour.getG():
+                neighbour.previous = current
+                neighbour.setG(value=tempG)
+                neighbour.setF(neighbour.getG() + neighbour.getH())
+                if neighbour not in openSet:
+                    openSet.append(neighbour)
+
+        # construct_path(openSet[len(openSet)-1])
 
 
 def main():
     global SOLVED
-    win = GraphWin("A*", WIDTH, HEIGHT)
+    global WIN
+    WIN = GraphWin("A*", WIDTH, HEIGHT)
 
     for i in range(ROWS):
         _ = []
@@ -108,78 +184,75 @@ def main():
         grid.append(_)
 
     
-    grid[int(START.x)][int(START.y)].start = True
-    grid[max(0, int(END.x)-1)][max(0, int(END.y)-1)].end = True
-    grid[int(START.x)][int(START.y)].obstacle = False
-    grid[max(0, int(END.x)-1)][max(0, int(END.y)-1)].obstacle = False
-
-    
     for i in range(ROWS):
         for j in range(COLS):
-            grid[i][j].show(win)
-
-    for i in range(ROWS):
-        for j in range(COLS):
-            grid[i][j].get_neighbours()
-            grid[i][j].setG()
-            grid[i][j].setH()
+            grid[i][j].show(WIN)
 
 
-    def draw_path(path):
-        # print(len(path))
-        for p in path:
-            p.undraw()
-            p.path = True
-            p.redraw(win)
-            # print(p.i, p.j)
-            # p.show(win)
 
-    def construct_path(current):
-        path = []
-        path.append(current)
-        current =  current
-        while current.previous:
-            path.insert(0, current.previous)
-            current = current.previous
-
-        draw_path(path)
-
-    
-    openSet = []
-    closedSet = []
-    openSet.append(grid[0][0])
-    
-    # print(len(openSet))
-    while len(openSet) > 0:
-        global SOLVED
         
-        current = openSet[0]
-        if current.i == max(0, END.x-1) and current.j == max(0, END.y-1):
-            SOLVED = True
-            construct_path(current)
-            
-        
-        openSet.remove(current)
+    while True:
+        k = WIN.getKey()
+        if k.lower() == "s":
+            m = WIN.getMouse()
+            for i in range(ROWS):
+                for j in range(COLS):
+                    if grid[i][j].contains(m):
+                        START.x , START.y = i, j
+                        grid[i][j].start = True
+                        grid[i][j].redraw(WIN)
 
-        for neighbour in current.neighbours:
-            
-            tempG = current.getG() + 1
+        if k.lower() == "e":
+            m = WIN.getMouse()
+            for i in range(ROWS):
+                for j in range(COLS):
+                    if grid[i][j].contains(m):
+                        END.x, END.y = i+1, j+1
+                        grid[i][j].end = True
+                        grid[i][j].redraw(WIN)
 
-            if tempG < neighbour.getG():
-                neighbour.previous = current
-                neighbour.setG(value=tempG)
-                neighbour.setF(neighbour.getG() + neighbour.getH())
+        if k.lower() == "w":
+            m = WIN.getMouse()
+            for i in range(ROWS):
+                for j in range(COLS):
+                    if grid[i][j].contains(m):
+                        grid[i][j].obstacle = True
+                        grid[i][j].redraw(WIN)
 
-                if neighbour not in openSet and not neighbour.obstacle:
-                    openSet.append(neighbour)
+        if k.lower() == "d":
+            m = WIN.getMouse()
+            for i in range(ROWS):
+                for j in range(COLS):
+                    if grid[i][j].contains(m):
+                        grid[i][j].obstacle = False
+                        grid[i][j].redraw(WIN)
 
-        # construct_path(openSet[len(openSet)-1])
 
-    if not SOLVED:
-        print("NO SOLUTION!!")
+        if k.lower() == "a":
+            for i in range(ROWS):
+                for j in range(COLS):
+                    grid[i][j].get_neighbours()
+                    grid[i][j].setG()
+                    grid[i][j].setH()
+            Astar()
+            # construct_path()
+            if not SOLVED:
+                print("NO SOLUTION!!")
 
-    win.getMouse()
-    win.close()
+        if k.lower() == "x":
+            try:
+                WIN.close()
+            except GraphicsError:
+                break
+
+
+
+    # Astar()
+    
+
+
+    # WIN.getMouse()
+    # WIN.close()
 
 
 
