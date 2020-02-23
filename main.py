1e42ae06c572851c03d1cd2a15b7649066ba50d0
Gabriel <rgabriel1508@gmial.com>
2020-02-23 16:26:44 +0530

    fixed bugs

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ HEIGHT = 600
 ROWS = 50
 COLS = 50
 START = Point(0, 0)
-END = Point(43, 50)
+END = Point(40, 30)
 WX = WIDTH/ROWS
 WY = HEIGHT/COLS
 
@@ -26,17 +26,34 @@ class Node():
         self.end = False
         self.previous = None
         self.neighbours = []
-        self.obstacle = True if random.random() < 0.2 else False
-        self.color = "black" if self.obstacle else "red"
+        self.obstacle = True if random.random() < 0.25 else False
+        self.path = False
+        self.color = self.getColor()
         self.r = Rectangle(Point(self.x, self.y), Point(self.x + WX-1, self.y + WY-1))
 
+    def getColor(self):
+        if self.start:
+            return color_rgb(0, 100, 150)
+        elif self.end:
+            return color_rgb(255, 0, 50)
+        if self.path:
+            return "green"
+        elif self.obstacle:
+            return "black"
+        else:
+            return "white"
 
     def show(self, win):
-        self.r.setFill(self.color)
+        self.r.setFill(self.getColor())
         self.r.draw(win)
+
 
     def undraw(self):
         self.r.undraw()
+
+    def redraw(self, win):
+        self.undraw()
+        self.show(win)
 
     def setColor(self, color):
         self.color = color
@@ -50,7 +67,7 @@ class Node():
     def getG(self): return self.g
 
     def setH(self): 
-        self.h = Node.distFrom(self, grid[int(END.x)-1][int(END.y)-1])
+        self.h = Node.distFrom(self, grid[max(0, int(END.x)-1)][max(0, int(END.y)-1)])
 
     def getH(self): return self.h
 
@@ -85,8 +102,10 @@ def main():
         grid.append(_)
 
     
-    grid[0][0].start = True
-    grid[ROWS-1][COLS-1].end = True
+    grid[int(START.x)][int(START.y)].start = True
+    grid[max(0, int(END.x)-1)][max(0, int(END.y)-1)].end = True
+    grid[int(START.x)][int(START.y)].obstacle = False
+    grid[max(0, int(END.x)-1)][max(0, int(END.y)-1)].obstacle = False
 
     
     for i in range(ROWS):
@@ -104,16 +123,17 @@ def main():
         print(len(path))
         for p in path:
             p.undraw()
+            p.path = True
+            p.redraw(win)
             # print(p.i, p.j)
             # p.show(win)
 
-    path = []
-    path.append(grid[int(END.x)-1][int(END.y)-1])
-    def construct_path():
-        
-        current =  grid[int(END.x)-1][int(END.y)-1]
+    def construct_path(current):
+        path = []
+        path.append(current)
+        current =  current
         while current.previous:
-            path.append(current.previous)
+            path.insert(0, current.previous)
             current = current.previous
 
         draw_path(path)
@@ -127,8 +147,9 @@ def main():
     while len(openSet) > 0:
         
         current = openSet[0]
-        if current.i == END.x-1 and current.j == END.y-1:
-            construct_path()
+        if current.i == max(0, END.x-1) and current.j == max(0, END.y-1):
+            construct_path(current)
+            
         
         openSet.remove(current)
 
@@ -143,6 +164,8 @@ def main():
 
                 if neighbour not in openSet and not neighbour.obstacle:
                     openSet.append(neighbour)
+
+        # construct_path(openSet[len(openSet)-1])
 
     
 
